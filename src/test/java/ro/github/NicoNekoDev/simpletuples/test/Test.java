7d57df65f818c3aaa92a9f189c9b3b25c7b41801
NicoNekoDev <nicuch100@gmail.com>
2021-09-21 11:17:58 +0300

    Test failed because I'm being stupid

--- src/test/java/ro/github/NicoNekoDev/simpletuples/test/Test.java
+++ src/test/java/ro/github/NicoNekoDev/simpletuples/test/Test.java
@@ -1,12 +1,8 @@
 package ro.github.NicoNekoDev.simpletuples.test;
 
-import org.github.NicoNekoDev.simpletuples.optional.OptionalPair;
-
 public class Test {
 
-    public void abc() {
-        OptionalPair<String, String> optionalPair = new OptionalPair<>("hello!", null);
-        optionalPair.toOptionalTriplet("").getFirstValue();
-
+    public void test() {
+        // TODO: later
     }
 }
